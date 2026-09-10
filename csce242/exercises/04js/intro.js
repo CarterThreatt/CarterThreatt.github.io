bad993c408d3a67f6962d209d7e0a4b33f46abf6
CarterThreatt <cartert@sc.edu>
2026-09-10 12:34:05 -0400

    in class exersises sep 10

--- csce242/exercises/04js/intro.js
+++ csce242/exercises/04js/intro.js
@@ -1,4 +1,17 @@
+//Shows a message when the button is clicked
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Button clicked!";
     e.target.innerHTML = "done!";
 };
+
+//styles the link when it is clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //don't go to links destination
+    e.target.classList.add("cool-link");
+}
+
+//animates the ball when the button is clicked
+document.getElementById("btn-start-animation").onclick = (e) => {
+    console.log("ball animation button clicked");
+    document.getElementById("ball").classList.toggle("bounce-animation");
+}
